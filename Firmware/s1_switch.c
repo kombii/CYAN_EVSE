b3a0a65777f2ead5afd57013311f7399f2abe09e
kombii <377981654@qq.com>
2021-10-09 09:59:36 +0800

    修正了一些拼写

--- Firmware/s1_switch.c
+++ Firmware/s1_switch.c
@@ -2,19 +2,21 @@
 #include "pwm.h"
 #include "config.h"
 
-void s1_switch_init()
+void S1_Switch_Init()
 {
 	PWM_config();
-	s1_switch_off();
+	S1_Switch_Off();
 	P14 = 1;
 }
 
-void s1_switch_on()
+void S1_Switch_On()
 {
 	PWMA_ENO3P_ENABLE();
 }
-void s1_switch_off()
+void S1_Switch_Off()
 {
 	PWMA_ENO3P_DISABLE();
+	//这里有个问题，在切换的瞬间，实际上会遇到刚下降就关闭pwm的情况，此时会获得一个巨大的占空比，这个对于车来说可能有问题
+	//不过规范里的S1也是直接硬切换的，切换期间也会有电压下落，车端应该对这个情况做判断，暂时先这么实现。
 	P14 = 1;
 }
--- Firmware/s1_switch.h
+++ Firmware/s1_switch.h
@@ -1,8 +1,8 @@
 #ifndef __S1_SWITCH_H
 #define __S1_SWITCH_H	 
 
-void s1_switch_init();
-void s1_switch_on();
-void s1_switch_off();
+void S1_Switch_Init();
+void S1_Switch_On();
+void S1_Switch_Off();
 	
 #endif
